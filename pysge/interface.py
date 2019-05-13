ac6ee3ddee4b0245be03946cd0d3fe4b09cad6a4
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-05-14 00:16:21 +0100

    Add name option to interface

--- pysge/interface.py
+++ pysge/interface.py
@@ -2,9 +2,9 @@ from .area import WorkingArea
 from .submitter import TaskSubmitter
 from .monitor import JobMonitor
 
-def submit(path, tasks=[], options="-q hep.q"):
+def submit(name, path, tasks=[], options="-q hep.q"):
     area = WorkingArea(path)
-    submitter = TaskSubmitter(options)
+    submitter = TaskSubmitter(" ".join([f'-N {name}', options]))
     monitor = JobMonitor(submitter)
 
     area.create_areas(tasks)
--- pysge/test_function.py
+++
@@ -1,6 +0,0 @@
-import time
-import numpy as np
-
-def function():
-    time.sleep(int(np.abs(np.random.normal(20, 20))))
-    return True
