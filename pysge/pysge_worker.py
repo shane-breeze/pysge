bbdedd66403b0b73b17d2bf0cffc7cbf93cc7c11
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-05-15 10:37:33 +0100

    Update pysge workers

--- pysge/pysge_worker.py
+++ pysge/pysge_worker.py
@@ -6,7 +6,7 @@ def main():
     with gzip.open("task.p.gz", 'rb') as f:
         task = pickle.load(f)
 
-    result = task()
+    result = task["task"](*task["args"], **task["kwargs"])
 
     with gzip.open("result.p.gz", 'wb') as f:
         pickle.dump(result, f)
