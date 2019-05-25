0ecbde9c82825f4067ca5bdab856b3c431835d51
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-05-25 08:49:43 +0100

    Update subprocess package

--- pysge/utils.py
+++ pysge/utils.py
@@ -1,6 +1,9 @@
-import subprocess as sp
+try:
+    import subprocess32 as sp
+except ModuleNotFoundError:
+    import subprocess as sp
 import shlex
 
 def run_command(cmd):
-    p = sp.Popen(shlex.split(cmd), stdout=sp.PIPE, stderr=sp.PIPE)
-    return p.communicate()
+    p = sp.run(shlex.split(cmd), stdout=sp.PIPE, stderr=sp.PIPE)
+    return p.stdout, p.stderr
