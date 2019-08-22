49fe4d857c47f9ab0fee9887a3125b2526aada00
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-08-22 15:02:16 +0100

    Add more docstrings

--- pysge/interface.py
+++ pysge/interface.py
@@ -9,7 +9,7 @@ from .monitor import JobMonitor
 
 logger = logging.getLogger(__name__)
 
-def validate_tasks(tasks):
+def _validate_tasks(tasks):
     for task in tasks:
         if any(key not in task for key in ("task", "args", "kwargs")):
             return False
@@ -55,7 +55,7 @@ def sge_submit(
         Minimum time between queries to the batch system.
 
     request_resubmission_options : bool (default = True)
-        When a job fails the master process will expect a stdin from the user
+        When a job fails the master process will expect an stdin from the user
         to alter the submission options (e.g. to increase walltime or memory
         requested). If False it will use the original options.
 
@@ -63,7 +63,7 @@ def sge_submit(
         Instead of opening the output files and loading them into python, just
         send the paths to the output files and let the user deal with them.
     """
-    if not validate_tasks(tasks):
+    if not _validate_tasks(tasks):
         logger.error(
             "Invalid tasks. Ensure tasks=[{'task': .., 'args': [..], "
             "'kwargs': {..}}, ...], where 'task' is callable."
@@ -94,17 +94,56 @@ def sge_submit(
     return results_not_files
 
 def sge_submit_yield(
-    name, path, tasks=[], options="-q hep.q", quiet=False, sleep=5,
+    tasks, label, tmpdir, options="-q hep.q", quiet=False, sleep=5,
     request_resubmission_options=True,
 ):
-    if not validate_tasks(tasks):
+    """
+    Submit jobs to an SGE batch system. No monitoring is perfomed and the
+    function returns an iterator over the results that yielding a list of the
+    results. Jobs still running are given None in the list and the iterator
+    terminates after all jobs are finished. This allows the user to perform
+    some processing on partial results.
+
+    Parameters
+    ----------
+    tasks : list
+        A list of dictrionaries with the keys: task, args and kwargs. Each
+        element is run on a node as task(*args, **kwargs).
+
+    label : str
+        Label given to the qsub submission script through -N.
+
+    tmpdir : str
+        Path to temporary directory (doesn't have to exist) where pysge stores
+        job infomation. Each call will have a unique identifier in the form
+        tpd_YYYYMMDD_hhmmss_xxxxxxxx. Within this directory exists all tasks in
+        separate directories with a dilled file, stdout and stderr for that
+        particular job.
+
+    options : str (default = "-q hep.q")
+        Additional options to pass to the qsub command. Take care since the
+        following options are already in use: -wd, -V, -e, -o and -t.
+
+    quiet : bool (default = False)
+        Don't print tqdm progress bars. Other prints are controlled by logging.
+
+    sleep : float (default = 5)
+        Minimum time between queries to the batch system.
+
+    request_resubmission_options : bool (default = True)
+        When a job fails the master process will expect an stdin from the user
+        to alter the submission options (e.g. to increase walltime or memory
+        requested). If False it will use the original options.
+    """
+
+    if not _validate_tasks(tasks):
         logger.error(
             "Invalid tasks. Ensure tasks=[{'task': .., 'args': [..], "
             "'kwargs': {..}}, ...], where 'task' is callable."
         )
         return []
-    area = WorkingArea(os.path.abspath(path))
-    submitter = SGETaskSubmitter(" ".join(['-N {}'.format(name), options]))
+    area = WorkingArea(os.path.abspath(tmpdir))
+    submitter = SGETaskSubmitter(" ".join(['-N {}'.format(label), options]))
     monitor = JobMonitor(submitter)
 
     area.create_areas(tasks, quiet=quiet)
@@ -114,11 +153,39 @@ def sge_submit_yield(
     )
 
 def sge_resume(
-    name, path, options="-q hep.q", quiet=False, sleep=5,
+    label, tmpdir, options="-q hep.q", quiet=False, sleep=5,
     request_resubmission_options=True,
 ):
-    area = WorkingArea(os.path.abspath(path), resume=True)
-    submitter = SGETaskSubmitter(" ".join(['-N {}'.format(name), options]))
+    """
+    Resubmit jobs based on the temporary directory (with the tpd_*
+    subdirectory). The original submission may terminate due to unforseen
+    reasons. This will continue where everything left off.
+
+    Parameters
+    ----------
+    label : str
+        Label given to the qsub submission script through -N.
+
+    tmpdir : str
+        Path to temporary directory created by an sge_submit or similar.
+
+    options : str (default = "-q hep.q")
+        Additional options to pass to the qsub command. Take care since the
+        following options are already in use: -wd, -V, -e, -o and -t.
+
+    quiet : bool (default = False)
+        Don't print tqdm progress bars. Other prints are controlled by logging.
+
+    sleep : float (default = 5)
+        Minimum time between queries to the batch system.
+
+    request_resubmission_options : bool (default = True)
+        When a job fails the master process will expect an stdin from the user
+        to alter the submission options (e.g. to increase walltime or memory
+        requested). If False it will use the original options.
+    """
+    area = WorkingArea(os.path.abspath(tmpdir), resume=True)
+    submitter = SGETaskSubmitter(" ".join(['-N {}'.format(label), options]))
     for idx in range(len(area.task_paths)):
         submitter.jobid_tasks['{}'.format(idx)] = area.task_paths[idx]
     monitor = JobMonitor(submitter)
@@ -133,7 +200,26 @@ def sge_resume(
     return results
 
 def mp_submit(tasks, ncores=4, quiet=False):
-    if not validate_tasks(tasks):
+    """
+    Submit multiprocessing jobs. Each task spawns a new child process which is
+    run on an available core. In multiprocessing mode the functions are
+    pickled and sent to each core, hence this is good at testing small scale
+    local submission before submitting to the batch. Return a list of the
+    results of each job (i.e. the return values of the function calls)
+
+    Parameters
+    ----------
+    tasks : list
+        A list of dictrionaries with the keys: task, args and kwargs. Each
+        element is run on a node as task(*args, **kwargs).
+
+    ncores : int
+        The number of cores to run on.
+
+    quiet : bool (default = False)
+        Don't print tqdm progress bars. Other prints are controlled by logging.
+    """
+    if not _validate_tasks(tasks):
         logger.error(
             "Invalid tasks. Ensure tasks=[{'task': .., 'args': [..], "
             "'kwargs': {..}}, ...], where 'task' is callable."
@@ -143,7 +229,19 @@ def mp_submit(tasks, ncores=4, quiet=False):
     return submitter.submit_tasks(tasks, ncores=ncores, quiet=quiet)
 
 def local_submit(tasks, quiet=False):
-    if not validate_tasks(tasks):
+    """
+    Submit local jobs. Mainly for testing purposes.
+
+    Parameters
+    ----------
+    tasks : list
+        A list of dictrionaries with the keys: task, args and kwargs. Each
+        element is run on a node as task(*args, **kwargs).
+
+    quiet : bool (default = False)
+        Don't print tqdm progress bars. Other prints are controlled by logging.
+    """
+    if not _validate_tasks(tasks):
         logger.error(
             "Invalid tasks. Ensure tasks=[{'task': .., 'args': [..], "
             "'kwargs': {..}}, ...], where 'task' is callable."
