da1ea9e7e90753806975cc1fb2260573d5f9811a
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-09-02 11:30:54 +0100

    Fix pysge worker timing

--- pysge/pysge_worker.sh
+++ pysge/pysge_worker.sh
@@ -3,4 +3,4 @@ ulimit -c 0
 taskid=$((SGE_TASK_ID - 1))
 path=task_$(printf "%05d" ${taskid})
 cd $path
-pysge_worker.py > stdout.txt 2> stderr.txt
+{ time pysge_worker.py ; } > $PWD/stdout.txt 2> $PWD/stderr.txt
