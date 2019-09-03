074dbcf93d86ca4b4e004319443b9924e91f26f0
Shane Breeze <s.breeze15@imperial.ac.uk>
2019-09-04 00:03:26 +0100

    Change back to submission directory to create result.p.lz4 file

--- pysge/pysge_worker.py
+++ pysge/pysge_worker.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import lz4.frame
 import dill
+import os
 
 def main():
+    cwd = os.getcwd()
     with lz4.frame.open("task.p.lz4", 'rb') as f:
         task = dill.load(f)
 
@@ -10,6 +12,9 @@ def main():
         task["task"], task["args"], task["kwargs"],
     ))
     result = task["task"](*task["args"], **task["kwargs"])
+
+    # Just incase the user wants to change directory within the task
+    os.chdir(cwd)
 
     with lz4.frame.open("result.p.lz4", 'wb') as f:
         dill.dump(result, f)
